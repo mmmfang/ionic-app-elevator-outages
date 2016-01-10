19d5624fd8ec4d494d0b599204eee2e7ab50764d
mmmfang <mae.fang@gmail.com>
2016-01-10 10:13:49 -0500

    proxy server linkedin ionic.project, added x2js plugin

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('mtaApp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,79 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller('OutageCtrl', ['$http', '$scope', function($http, $scope){
+
+ var ctrl=this;
+  
+  this.makeAPICall = function(){
+    $.get('/feed', function(xml){
+
+      // Create x2js instance with default config
+      var x2js = new X2JS();
+      var xmlText = "<MyRoot><test>Success</test><test2><item>val1</item><item>val2</item></test2></MyRoot>";
+      var jsonObj = x2js.xml_str2json( xmlText );
+
+        var json = $.xml2json(xml); //json will get all the json
+        ctrl.outage = json.outage; //gets outages as objects in an array
+    });
+   };
+  this.makeAPICall();
+
+}])
+
+
+//angular.module('mtaAppService',[])
+    .factory('DataSource', ['$http',function($http){
+       return {
+           get: function(callback){
+                $http.get('/feed', {transformResponse:function(data) {
+                      // convert the data to JSON and provide
+                      // it to the success function below
+                        var x2js = new X2JS();
+                        var json = x2js.xml_str2json( data );
+                        return json;
+                        }
+                    }
+                ).
+                success(function(data, status) {
+                    // send the converted data back
+                    // to the callback function
+                    callback(data);
+                })
+           }
+       }
+    }]);
+
+//http://www.clearlyinnovative.com/working-xml2json-strings-ionic-framework-project
+
+ 
+var AppController = function($scope,DataSource) {
+     
+    //This is the callback function
+    setData = function(data) {
+        console.log("setdata", data);
+        $scope.dataSet = data;
+    }
+         
+    DataSource.get(setData);
+  };   
+
+// var AppController = function($scope,DataSource) {
+
+//   var SOURCE_FILE = "timer.xml";
+
+//   xmlTransform = function(data) {
+//     console.log("transform data");
+//     var x2js = new X2JS();
+//     var json = x2js.xml_str2json(data);
+//     return json.TimerStatus;
+//   };
+
+//   setData = function(data) {
+//     console.log("setdata", data);
+//     $scope.dataSet = data;
+//   };
+
+//   DataSource.get(SOURCE_FILE,setData,xmlTransform);
+// };
---
+++ www/js/services.js
@@ -0,0 +1,53 @@
+//angular.module('mtaAppService',[])
+    .factory('DataSource', ['$http',function($http){
+       return {
+           get: function(callback){
+                $http.get('/feed', {transformResponse:function(data) {
+                      // convert the data to JSON and provide
+                      // it to the success function below
+                        var x2js = new X2JS();
+                        var json = x2js.xml_str2json( data );
+                        return json;
+                        }
+                    }
+                ).
+                success(function(data, status) {
+                    // send the converted data back
+                    // to the callback function
+                    callback(data);
+                })
+           }
+       }
+    }]);
+
+//http://www.clearlyinnovative.com/working-xml2json-strings-ionic-framework-project
+
+ 
+var AppController = function($scope,DataSource) {
+     
+    //This is the callback function
+    setData = function(data) {
+        $scope.dataSet = data;
+    }
+         
+    DataSource.get(setData);
+     
+
+var AppController = function($scope,DataSource) {
+
+  var SOURCE_FILE = "timer.xml";
+
+  xmlTransform = function(data) {
+    console.log("transform data");
+    var x2js = new X2JS();
+    var json = x2js.xml_str2json(data);
+    return json.TimerStatus;
+  };
+
+  setData = function(data) {
+    console.log("setdata", data);
+    $scope.dataSet = data;
+  };
+
+  DataSource.get(SOURCE_FILE,setData,xmlTransform);
+};
