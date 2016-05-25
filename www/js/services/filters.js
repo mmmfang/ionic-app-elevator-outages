adb947af2df575a26f2b9ff7e86bf883e4ba2fb8
mmmfang <mae.fang@gmail.com>
2016-05-24 20:27:47 -0400

    added new service for word filters

---
+++ www/js/services/filters.js
@@ -0,0 +1,33 @@
+angular.module('mtaApp.Filters', []);
+
+app.service('myService', function() {
+  return {
+    elorEsc: function(word){
+        var charZero = word.charAt(0);
+        var charOne = word.charAt(1);
+
+        if (charZero=='E' && charOne=='L') {
+          return "Elevator";
+        } else {
+          return "Escalator";
+        }
+    },
+    filterBoro: function (word) {
+        var charCero = word.charAt(0);
+        var charUno = word.charAt(1);
+
+        if (charCero=='M' && charUno=='N') {
+          return "Manhattan";
+        } else if (charCero=='Q' && charUno=='N') {
+          return "Queens";
+        } else if (charCero=='B' && charUno=='K') {
+          return "Brooklyn"
+        } else if (charCero=='B' && charUno=='X') {
+          return "Bronx"
+        } else {
+          return "N/A"
+      	;
+     	};
+    });
+});
+
