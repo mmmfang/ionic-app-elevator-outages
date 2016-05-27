93fa54de49e3a7765d8afa0bb2249443f8a2bb0a
mmmfang <mae.fang@gmail.com>
2016-05-26 23:56:58 -0400

    deleted some comments

--- www/js/app.js
+++ www/js/app.js
@@ -34,8 +34,6 @@ app.config(function($stateProvider, $urlRouterProvider){
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
       // stops the viewport from snapping when text inputs are focused. 
@@ -59,33 +57,18 @@ app.controller('OutageCtrl', function($scope, XmlConverter) {
     XmlConverter.get(outageData);
 });
 
-app.controller('HomeCtrl', function($scope, Filters) {
-     
-$scope.home = "home";
-
+app.controller('HomeCtrl', function($scope, Filters) {   
+  $scope.home = "home";
 });
 
-
 app.controller('BoroCtrl', function($scope, XmlConverter) {
-
-    //This is the callback function
     outageData = function(data) {
         $scope.outages = data.NYCOutages.outage;
-        //console.log($scope.outages);
     };
 
     XmlConverter.get(outageData);
-
 });
 
-
-
-///PLANNING
-//SEARCHCTRL?? by borough, train line, station(search)
-//put the search stuff in a factory?
-//return{boroSearch: function(clickedonboro) {}, trainLineSearch: function(){}, typeSearch(){}}
-//put custom filters in a directive
-// app.factory('properName': function(char) {},) FOR THOSE EL ESC, BOROUGH SEARCH
 
 app.controller('TrainlineCtrl', function($scope, XmlConverter) {
   
@@ -97,29 +80,15 @@ app.controller('TrainlineCtrl', function($scope, XmlConverter) {
 
 app.controller('StationCtrl', function($scope, XmlConverter) {
 
-    //This is the callback function
     outageData = function(data) {
-        //console.log("outageData", data.NYCOutages.outage); //array of 40 objects
         $scope.outages = data.NYCOutages.outage;
     };
 
     XmlConverter.get(outageData);
-
 });
 
-app.controller('HomeCtrl', function($scope, XmlConverter) {
 
-//     //This is the callback function
-//     outageData = function(data) {
-//         //console.log("outageData", data.NYCOutages.outage); //array of 40 objects
-//         $scope.outages = data.NYCOutages.outage;
-//     };
-
-//     XmlConverter.get(outageData);
-
-});
-
-//ANGULAR CUSTOM FILTERS - TO DO: See how ionic handles angular custom filters
+//ANGULAR CUSTOM FILTERS
 
 //To display whether elevator or escalator is broken
 app.filter('equipmentFilter', function() {
