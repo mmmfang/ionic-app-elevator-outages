2ee99640da6d20d7858cfac8da1b6e80fef9e30f
mmmfang <mae.fang@gmail.com>
2016-07-22 18:54:18 -0400

    cleaned up spacing

--- www/js/app.js
+++ www/js/app.js
@@ -30,61 +30,37 @@ app.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/home');
 });
 
-
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // stops the viewport from snapping when text inputs are focused. 
-      // Ionic handles this internally for a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-});
-
-
-app.controller('OutageCtrl', function($scope, XmlConverter) {
-
-    var outageData = function(data) {
-        //console.log("outageData", data.NYCOutages.outage); //array of 40 objects
-        $scope.outages = data.NYCOutages.outage;
-    };
-
+app.controller('OutageCtrl', function($scope, XmlConverter){
+  var outageData = function(data) {
+    //console.log("outageData", data.NYCOutages.outage); //array of 40 objects
+    $scope.outages = data.NYCOutages.outage;
+  };
     XmlConverter.get(outageData);
-
-
 });
 
-app.controller('HomeCtrl', function($scope) {   
+app.controller('HomeCtrl', function($scope){
   $scope.home = "home";
 });
 
-app.controller('BoroCtrl', function($scope, XmlConverter) {
-    outageData = function(data) {
-        $scope.outages = data.NYCOutages.outage;
-    };
-
+app.controller('BoroCtrl', function($scope, XmlConverter){
+  var outageData = function(data) {
+    $scope.outages = data.NYCOutages.outage;
+  };
     XmlConverter.get(outageData);
 });
 
 
 app.controller('TrainlineCtrl', function($scope, XmlConverter) {
-  
-  outageData=function(data) {
-    $scope.outages = data.NYCOutages.outage;  
-  };     
+  var outageData=function(data) {
+    $scope.outages = data.NYCOutages.outage;
+  };
     XmlConverter.get(outageData);
 });
 
 app.controller('StationCtrl', function($scope, XmlConverter) {
-    outageData = function(data) {
-        $scope.outages = data.NYCOutages.outage;
-    };
-
+  outageData = function(data) {
+    $scope.outages = data.NYCOutages.outage;
+  };
     XmlConverter.get(outageData);
 });
 
@@ -101,7 +77,7 @@ app.filter('equipmentFilter', function() {
         } else {
           return "Escalator";
         }
-     }
+     };
 });
 
 //To display full borough name from abbreviation provided
@@ -109,18 +85,18 @@ app.filter('boroFilter', function() {
    return function(word) {
    var charZero = word.charAt(0);
    var charOne = word.charAt(1);
-        if (charZero=='M' && charOne=='N') {
-          return "Manhattan";
-        } else if (charZero=='Q' && charOne=='N') {
-          return "Queens";
-        } else if (charZero=='B' && charOne=='K') {
-          return "Brooklyn"
-        } else if (charZero=='B' && charOne=='X') {
-          return "Bronx"
-        } else {
-          return "N/A"
-        }
-     }
+      if (charZero=='M' && charOne=='N') {
+        return "Manhattan";
+      } else if (charZero=='Q' && charOne=='N') {
+        return "Queens";
+      } else if (charZero=='B' && charOne=='K') {
+        return "Brooklyn";
+      } else if (charZero=='B' && charOne=='X') {
+        return "Bronx";
+      } else {
+        return "N/A";
+      }
+   };
 });
 
 //CUSTOM DIRECTIVE
@@ -129,10 +105,23 @@ app.directive('outageListing', function(){
     template: '<ng-include src="getTemplateUrl()" />',
     controller: function ($scope) {
       $scope.getTemplateUrl = function(){
-        return "../templates/outage.html"
-      }
+        return "../templates/outage.html";
+      };
     }
-  }
-})
+  };
+});
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
 
 }()); //IIFE
